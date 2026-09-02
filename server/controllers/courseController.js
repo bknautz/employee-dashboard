5c3236f9650c88482806d154a6fd0fa01a530813
bknautz <brandon.knautz@gmail.com>
2026-09-02 18:52:53 -0500

    finished show all courses and add course

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,20 +1,63 @@
-const Course = require('../models/Course');
-
+const Course = require("../models/Course");
 
 // GET /api/courses
 exports.getAllCourses = async (req, res) => {
-     try {
-       const results = await Course.find();
-       res.status(201).json({
-      message: 'User created',
-      results
+  try {
+    const results = await Course.find();
+    res.status(200).json({
+      message: "Courses Retrieved",
+      results,
     });
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // POST /api/courses
 exports.createCourse = async (req, res) => {
+  try {
+    const { title, provider, description, hours, expirationMonths } = req.body;
 
+    if (
+      typeof title !== 'string' ||
+      typeof provider !== 'string' ||
+      typeof hours !== 'number' ||
+      !Number.isInteger(hours) ||
+      (description !== undefined && typeof description !== 'string') ||
+      (expirationMonths !== undefined && expirationMonths !== null && typeof expirationMonths !== 'number')
+    ) {
+      return res.status(400).json({ error: 'Invalid Course Input' });
+    }
+
+    const existingCourse = await Course.findOne({ title });
+    if (existingCourse) {
+      return res.status(400).json({ error: 'Course already exists' });
+    }
+
+    const course = new Course({
+      title,
+      provider,
+      description,
+      hours,
+      expirationMonths,
+    });
+
+    await course.save();
+
+    
+    res.status(201).json({
+      message: 'Course created',
+      
+      course: {
+        id: course._id,
+        title: course.title,
+        provider: course.provider,
+        description: course.description,
+        hours: course.hours,
+        expirationMonths: course.expirationMonths,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
