7ad8dda3d4360307b7a236bd40579503cd23750f
bknautz <brandon.knautz@gmail.com>
2026-09-02 19:13:32 -0500

    completed put request for courses

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -61,3 +61,32 @@ exports.createCourse = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// GET /api/courses/:id
+exports.getCourseById = async (req, res) => {
+    try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).json({ error: 'Course Not Found' });
+    }
+    res.status(200).json({
+      message: "Course Retrieved",
+      course,
+    });
+  } catch (err) {
+    if (err.name === 'CastError') {
+        res.status(400).json({error: "Invalid Course ID"});
+      } else {
+      res.status(500).json({ error: err.message });
+    }
+}};
+
+// PUT /api/courses/:id
+exports.updateCourse = async (req, res) => {
+
+};
+
+// DELETE /api/courses/:id
+exports.deleteCourse = async (req, res) => {
+
+};
